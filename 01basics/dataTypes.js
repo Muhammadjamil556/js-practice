356ee5777b044ec1e00a747a7c99925338959688
Muhammad jamil <81313400+Muhammadjamil556@users.noreply.github.com>
2024-10-17 10:54:04 +0000

    data types

---
+++ 01basics/dataTypes.js
@@ -0,0 +1,18 @@
+"user strict"; // treat all js code as newer version 
+
+//alert(3 +3) // we are using node js, not browser
+
+
+let name ="jamil" //string data type
+let age = 18 //num data type 2 to 53
+let islogged =false //boolean true/false
+let state=null
+let temp; //undefined 
+//null => standalone value 
+//undefined =>  
+    //symbols=>unique
+//object 
+
+
+console.log(typeof null)
+console.log(typeof undefined)

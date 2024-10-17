b154b602555ba35079c134f5286d7fb310b61b56
Muhammad jamil <81313400+Muhammadjamil556@users.noreply.github.com>
2024-10-17 11:12:59 +0000

    coversion Operations

---
+++ 01basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber )
+console.log(valueInNumber )
+
+
+// "33" => 33
+// "33abc"=>NaN
+// true => 1 ; false =>0
+
+let isloggedIn =""
+let isBooleanLoggedIn=Boolean(isloggedIn)
+
+console.log(isBooleanLoggedIn )
+
+
+// 1=> true; 0=> false 
+//""=>false;
+//"jamil"=>true
+
+let someNumber = 33
+let isSomeNumberString=String(someNumber)
+console.log(isSomeNumberString)
+console.log(typeof isSomeNumberString)
